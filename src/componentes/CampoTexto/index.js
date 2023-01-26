13dd6feb03ab9081e42d521c2cf7bef9e1ddb1ad
Natalie Cabral <nataliegc23@gmail.com>
2023-01-26 19:23:41 -0300

    update

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,20 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}`
+
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input type="text" placeholder={props.placeholder}required={props.obrigatorio}></input>
+            <input onChange={aoDigitado} type="text" placeholder={placeholderModificada}required={props.obrigatorio}></input>
         </div>
     )
 }
