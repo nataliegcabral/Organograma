4ae88146c95994524d4af6f91d3ff086d6e60d6c
Natalie Cabral <nataliegc23@gmail.com>
2023-01-26 19:09:00 -0300

    update

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner () {
+// JSX - é como o React lê o código e transforma em elementos no DOM
+    return (
+        <header className="banner">
+            <img src="/img/banner.png" alt="Banner principal da página do Organo" />
+            </header>
+    )
+}
+
+export default Banner
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,40 @@
+import Botao from '../Botao';
+import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import './Formulario.css';
+
+const Formulario = () => {
+
+    const times = [
+        '',
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX & Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('form foi submetido')
+    }
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
+            <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
+            <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+            <Botao texto="Criar Card">
+                Criar Card
+            </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
