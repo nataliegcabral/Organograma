d05143131b8a78e36b86d807ece7e8aa15ec47aa
Natalie Cabral <nataliegc23@gmail.com>
2023-02-20 18:15:17 -0300

    update

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,11 +1,21 @@
 import './Banner.css'
 
-function Banner () {
-// JSX - é como o React lê o código e transforma em elementos no DOM
+function Banner() {
+    // JSX - é como o React lê o código e transforma em elementos no DOM
     return (
         <header className="banner">
-            <img src="img/banner.png" alt=""/>
-            </header>
+
+            <div className='title'>
+
+            <img src='img/logo.png' alt='' className='logo'/>   
+                <h1>Jogos e Times</h1>
+                <p>Organizados em um só lugar</p>
+                
+            </div>
+
+            <img src="img/pessoas.png" alt="" className='pessoas'/>
+
+        </header>
     )
 }
 
